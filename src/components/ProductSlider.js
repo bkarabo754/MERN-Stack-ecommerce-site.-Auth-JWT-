9ce469390a84ad299ddfe7d3c24d82a8497db6bd
Bongani Masango <bkmasango26@gmail.com>
2023-04-11 18:34:12 +0200

    authentication

--- src/components/ProductSlider.js
+++ src/components/ProductSlider.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import {Swiper, SwiperSlide} from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import {Pagination, Navigation} from 'swiper'
+import Product from '../components/Product'
 
-const ProductSlider = () => {
-  return <div>ProductSlider</div>;
+const ProductSlider = ({data}) => {
+  return (
+  <Swiper>
+    <>
+    {data?.map((product) => {
+      return (
+      <SwiperSlide key={product.id}>
+        <Product />
+      </SwiperSlide>
+      )
+    })}
+    </>
+  </Swiper>
+  );
 };
 
 export default ProductSlider;
